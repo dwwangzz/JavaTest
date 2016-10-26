ebfa19586714f740c897bc71a37da40c808dfbea
奈斯曼 <81303012@qq.com>
2016-10-26 17:13:27 +0800

    String的format方法

---
+++ src/main/java/string/StringFormat.java
@@ -0,0 +1,16 @@
+package string;
+
+/**
+ * Created by wangzz on 2016/10/26.
+ * String的format方法 - 还有许多用法需要研究
+ */
+public class StringFormat {
+
+    public static void main(String[] args) {
+
+        //保留两位小数
+        System.out.println(String.format("%.2f", 1234.2343));
+        //%.2f %.表示 小数点前任意位数 2 表示两位小数 格式后的结果为f 表示浮点型
+
+    }
+}
