a0a4f899314d1b7f5469b63dad5f9177c6f88600
奈斯曼 <81303012@qq.com>
2016-12-19 09:43:31 +0800

    测试类

--- src/main/java/Test/Test.java
+++ src/main/java/Test/Test.java
@@ -1,10 +1,30 @@
 package Test;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Test {
 
-	public static void main(String[] args) throws Exception {
-        String separator = System.getProperty("line.separator", "\n");
-        System.out.println(separator);
+    public static void main(String[] args) throws Exception {
+        String str = "18653151533,显示签收未收到,售后客服注意,售中客服注意，确认换货/补发订单";
+
+        System.out.println(getTelnum(str));
+    }
+
+    public static String getTelnum(String sParam) {
+        if (sParam.length() <= 0)
+            return "";
+        Pattern pattern = Pattern.compile("(\\+?86)?1\\d{10}$*");
+        Matcher matcher = pattern.matcher(sParam);
+        StringBuffer bf = new StringBuffer();
+        while (matcher.find()) {
+            bf.append(matcher.group()).append(",");
+        }
+        int len = bf.length();
+        if (len > 0) {
+            bf.deleteCharAt(len - 1);
+        }
+        return bf.toString();
     }
 
 }
