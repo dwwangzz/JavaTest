750215f00f2699e4b7d74611021d34426a946f5d
奈斯曼 <81303012@qq.com>
2016-12-19 09:43:17 +0800

    格式化代码

--- src/main/java/httpClientUtil/HttpClientTest.java
+++ src/main/java/httpClientUtil/HttpClientTest.java
@@ -8,8 +8,6 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
 
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 
 public class HttpClientTest {
@@ -20,7 +18,9 @@ public class HttpClientTest {
         //HttpClient_wangzz
         CloseableHttpClient closeableHttpClient = httpClientBuilder.build();
 
-        HttpGet httpGet = new HttpGet("https://rate.taobao.com/user-rate-UvCxGOmHuOF8u.htm");
+        //String url = "https://rate.taobao.com/user-rate-UvCxGOmHuOF8u.htm";
+        String url = "http://125.64.210.190:8080/znitPlatform/TaskPerson/infos?dealingId=856A57346256945CA5863321461A3F0A&&taskId=02b366d4f6fc46cba11064bdf7cb05eb&&idcards=512534197407295029,370102198003130037,511528199209051817,511321199310236458,532101199102104619,511523198502010612,511528199301151853,511528198701010414";
+        HttpGet httpGet = new HttpGet(url);
         System.out.println(httpGet.getRequestLine());
         try {
             //执行get请求  
@@ -35,12 +35,12 @@ public class HttpClientTest {
                 String htmlContent = EntityUtils.toString(entity);
                 //htmlContent = new String(htmlContent.getBytes("utf-8"),"gbk");
                 System.out.println("response content: \r\n" + htmlContent);
-                File file = new File("D:\\韩都衣舍.html");
+                //File file = new File("D:\\韩都衣舍.html");
                 // 创建一个FileWriter对象，该对象一被初始化，必须要明确被错左的文件
-                FileWriter fw = new FileWriter(file);
-                fw.write(htmlContent);
-                fw.flush();
-                fw.close();
+                //FileWriter fw = new FileWriter(file);
+                //fw.write(htmlContent);
+                //fw.flush();
+                //fw.close();
 
             }
         } catch (IOException e) {
--- src/main/java/reflect/ReflectDemo.java
+++ src/main/java/reflect/ReflectDemo.java
@@ -15,93 +15,93 @@ import java.util.List;
  * @author wangzz-a
  * @version $Id: ReflectDemo.java, v 0.1 2015年8月5日 下午4:38:26 wangzz-a Exp $
  */
-@SuppressWarnings({ "unchecked", "rawtypes" })
+@SuppressWarnings({"unchecked", "rawtypes"})
 public class ReflectDemo {
 
-	public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
 
-	}
+    }
 
-	
-	/** 
-	 * 加载类 - 获得类字节码的几种方式（获取类类型的方法）
-	 */
-	@Test
-	public void getPersionClass() throws ClassNotFoundException {
-		//获得类字节码的几种方式
-		//1.
-		Class clazz = Class.forName("reflect.entity.Person");
-		System.out.println(clazz);
-		//2.
-		Class p = new Person().getClass();
-		System.out.println(p);
-		//3.
-		Class c = Person.class;
-		System.out.println(c);
-	}
-	
-	@Test
-	public void test1() throws Exception{
-		//反射没有参数的构造函数
-		Class clazz = Person.class;
-		Constructor c = clazz.getConstructor(null);
-		Person p = (Person) c.newInstance(null);
-		System.out.println(p);
-	}
-	
-	@Test
-	public void test2() throws Exception{
-		//反射有一个参数的构造函数
-		Class clazz = Person.class;
-		Constructor c = clazz.getConstructor(String.class);
-		Person p = (Person) c.newInstance("小明");
-		System.out.println(p);
-		System.out.println(p.str);
-	}
-	
-	@Test
-	public void test3() throws Exception{
-		//反射有两个参数的构造函数
-		Class clazz = Person.class;
-		Constructor c = clazz.getConstructor(String.class,int.class);
-		Person p = (Person) c.newInstance("小明",18);
-		System.out.println(p);
-		System.out.println(p.str);
-	}
-	
-	@Test
-	public void test4() throws Exception{
-		//反射有集合参数的构造函数
-		Class clazz = Person.class;
-		System.out.println(List.class);
-		Constructor c = clazz.getConstructor(List.class);
-		//Person p = (Person) c.newInstance(new ArrayList());
-		//String [] arr = {"a","b","c"};
-		//Person p = (Person) c.newInstance(Arrays.asList(arr));
-		Person p = (Person) c.newInstance(new ArrayList<String>(Arrays.asList(new String[]{"A","B","C","D"})));
-		System.out.println(p);
-		System.out.println(p.str);
-	}
-	
-	@Test
-	public void test5() throws Exception{
-		//反射私有构造函数
-		Class clazz = Person.class;
-		Constructor c = clazz.getDeclaredConstructor(int.class);
-		//暴力反射
-		c.setAccessible(true);
-		Person p = (Person) c.newInstance(17);
-		System.out.println(p);
-		System.out.println(p.str);
-	}
-	
-	@Test
-	public void test6() throws Exception{
-		//创建对象的另外一种途径
-		Class clazz = Class.forName("reflect.entity.Person");
-		Person p = (Person) clazz.newInstance();
-		System.out.println(p);
-		System.out.println(p.str);
-	}
+
+    /**
+     * 加载类 - 获得类字节码的几种方式（获取类类型的方法）
+     */
+    @Test
+    public void getPersionClass() throws ClassNotFoundException {
+        //获得类字节码的几种方式
+        //1.
+        Class clazz = Class.forName("reflect.entity.Person");
+        System.out.println(clazz);
+        //2.
+        Class p = new Person().getClass();
+        System.out.println(p);
+        //3.
+        Class c = Person.class;
+        System.out.println(c);
+    }
+
+    @Test
+    public void test1() throws Exception {
+        //反射没有参数的构造函数
+        Class clazz = Person.class;
+        Constructor c = clazz.getConstructor(null);
+        Person p = (Person) c.newInstance(null);
+        System.out.println(p);
+    }
+
+    @Test
+    public void test2() throws Exception {
+        //反射有一个参数的构造函数
+        Class clazz = Person.class;
+        Constructor c = clazz.getConstructor(String.class);
+        Person p = (Person) c.newInstance("小明");
+        System.out.println(p);
+        System.out.println(p.str);
+    }
+
+    @Test
+    public void test3() throws Exception {
+        //反射有两个参数的构造函数
+        Class clazz = Person.class;
+        Constructor c = clazz.getConstructor(String.class, int.class);
+        Person p = (Person) c.newInstance("小明", 18);
+        System.out.println(p);
+        System.out.println(p.str);
+    }
+
+    @Test
+    public void test4() throws Exception {
+        //反射有集合参数的构造函数
+        Class clazz = Person.class;
+        System.out.println(List.class);
+        Constructor c = clazz.getConstructor(List.class);
+        //Person p = (Person) c.newInstance(new ArrayList());
+        //String [] arr = {"a","b","c"};
+        //Person p = (Person) c.newInstance(Arrays.asList(arr));
+        Person p = (Person) c.newInstance(new ArrayList<String>(Arrays.asList(new String[]{"A", "B", "C", "D"})));
+        System.out.println(p);
+        System.out.println(p.str);
+    }
+
+    @Test
+    public void test5() throws Exception {
+        //反射私有构造函数
+        Class clazz = Person.class;
+        Constructor c = clazz.getDeclaredConstructor(int.class);
+        //暴力反射
+        c.setAccessible(true);
+        Person p = (Person) c.newInstance(17);
+        System.out.println(p);
+        System.out.println(p.str);
+    }
+
+    @Test
+    public void test6() throws Exception {
+        //创建对象的另外一种途径
+        Class clazz = Class.forName("reflect.entity.Person");
+        Person p = (Person) clazz.newInstance();
+        System.out.println(p);
+        System.out.println(p.str);
+    }
 
 }
