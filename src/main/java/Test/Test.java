a0a35d9b381c47486458d792d835bb05c10fb24f
奈斯曼 <81303012@qq.com>
2017-03-23 11:38:04 +0800

    时间测试

--- src/main/java/Test/Test.java
+++ src/main/java/Test/Test.java
@@ -1,29 +1,16 @@
 package Test;
 
-import domain.Demo;
+import utils.DateUtil;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.Date;
 
 public class Test {
 
     public static void main(String[] args) throws Exception {
-
-        Demo demo = new Demo();
-        List<String> list = new ArrayList<>();
-        list.add("yz_hstyleqjd");
-        list.add("tb_prospecsqjd");
-        list.add("bb_soulineqjd");
-        list.add("bb_nibbunsqjd");
-        list.add("sb_honeypigqjd");
-        list.add("bb_gyqjd");
-        list.add("ccj_hstyleqjd");
-        list.add("wd_prospecsjb");
-        list.add("jd_soulinfsqjd");
-        demo.setCodes(list);
-        Collections.sort(list);
-        System.out.println(demo.toString());
+        Date date = new Date(2017 - 1900, 4, 30);
+        System.out.println(DateUtil.dateToStr(date, "yyyy-MM-dd"));
+        date = DateUtil.addMonth(date, -1);
+        System.out.println(DateUtil.dateToStr(date, "yyyy-MM-dd"));
     }
 
 }
