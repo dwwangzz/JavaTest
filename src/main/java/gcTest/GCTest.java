be9b338ab74e2d6f326d854d35fe492ce16e1cff
wangzz <81303012@qq.com>
2020-12-04 13:06:19 +0800

    GCTest代码优化

--- src/main/java/gcTest/GCTest.java
+++ src/main/java/gcTest/GCTest.java
@@ -5,28 +5,31 @@ import java.util.*;
 public class GCTest {
 
 	public static void main(String[] args) {
-		System. out .println( " 内存信息 :" + toMemoryInfo ());
+		System.out.println(" 内存信息 :" + toMemoryInfo());
 		oom();
 	}
-	
-	private static void oom(){
-        List<Map<String,Student>> list = new ArrayList<Map<String,Student>>();
-        while(true){
-        	Map<String, Student> map = new HashMap<String, Student>();
-            for(long j=0;j<=10000000;j++){
-            	Student stu = new Student(new Date().toString()+"name"+j, j++);
-            	map.put("key"+j, stu);
-            	System.out.println(j);
-            	System. out .println( " 内存信息 :" + toMemoryInfo ());
-            }
-            list.add(map);
-        }
-    }
-	/**获取当前 jvm 的内存信息*/
-    public static String toMemoryInfo() {
-        Runtime currRuntime = Runtime.getRuntime ();
-        int nFreeMemory = ( int ) (currRuntime.freeMemory() / 1024 / 1024);
-        int nTotalMemory = ( int ) (currRuntime.totalMemory() / 1024 / 1024);
-        return nFreeMemory + "M/" + nTotalMemory + "M(free/total)" ;
-     }
+
+	private static void oom() {
+		List<Map<String, Student>> list = new ArrayList<Map<String, Student>>();
+		while (true) {
+			Map<String, Student> map = new HashMap<String, Student>();
+			for (long j = 0; j <= 10000000; j++) {
+				Student stu = new Student(new Date().toString() + "name" + j, j++);
+				map.put("key" + j, stu);
+				System.out.println(j);
+				System.out.println(" 内存信息 :" + toMemoryInfo());
+			}
+			list.add(map);
+		}
+	}
+
+	/**
+	 * 获取当前 jvm 的内存信息
+	 */
+	public static String toMemoryInfo() {
+		Runtime currRuntime = Runtime.getRuntime();
+		int nFreeMemory = (int) (currRuntime.freeMemory() / 1024 / 1024);
+		int nTotalMemory = (int) (currRuntime.totalMemory() / 1024 / 1024);
+		return nFreeMemory + "M/" + nTotalMemory + "M(free/total)";
+	}
 }
