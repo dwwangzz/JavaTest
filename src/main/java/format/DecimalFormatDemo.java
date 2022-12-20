ae5253f8b5b9d51909a1bf8fd9d8fbab69c680b7
wangzz <81303012@qq.com>
2022-12-20 14:13:22 +0800

    [docs] 打开注释

--- src/main/java/format/DecimalFormatDemo.java
+++ src/main/java/format/DecimalFormatDemo.java
@@ -46,14 +46,14 @@ public class DecimalFormatDemo {
 
 
 
-        //// 取一位整数和两位小数
-        //System.out.println(new DecimalFormat("0.00").format(pi));//3.14
-        //// 取两位整数和三位小数，整数不足部分以0填补。
-        //System.out.println(new DecimalFormat("00.000").format(pi));// 03.142
-        //// 取所有整数部分
-        //System.out.println(new DecimalFormat("#").format(pi));//3
-        //// 以百分比方式计数，并取两位小数
-        //System.out.println(new DecimalFormat("#.##%").format(pi));//314.16%
+        // 取一位整数和两位小数
+        System.out.println(new DecimalFormat("0.00").format(pi));//3.14
+        // 取两位整数和三位小数，整数不足部分以0填补。
+        System.out.println(new DecimalFormat("00.000").format(pi));// 03.142
+        // 取所有整数部分
+        System.out.println(new DecimalFormat("#").format(pi));//3
+        // 以百分比方式计数，并取两位小数
+        System.out.println(new DecimalFormat("#.##%").format(pi));//314.16%
     }
 
 }
